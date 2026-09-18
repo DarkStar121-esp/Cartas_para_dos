b8491b32bfb7969e2ca691ecea98878ef7f3af6b
DarkStar121-esp <villarruelsantyago@gmail.com>
2026-09-18 09:15:19 -0300

    Agregar getter de color al modelo Gender

--- lib/models/gender.dart
+++ lib/models/gender.dart
@@ -16,3 +16,14 @@ enum Gender {
         _ => '#E0E0E0',
       };
 }
+
+import 'package:flutter/material.dart';
+
+extension GenderColorFix on Gender {
+  Color get color {
+    final str = toString().toLowerCase();
+    if (str.contains('female') || str.contains('mujer')) return Colors.pinkAccent;
+    if (str.contains('male') || str.contains('hombre')) return Colors.blueAccent;
+    return Colors.purpleAccent;
+  }
+}
