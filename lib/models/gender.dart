bfafa45d4212d10c429b356364ac1b970ef7a89c
DarkStar121-esp <villarruelsantyago@gmail.com>
2026-09-18 09:26:42 -0300

    Corregir ubicacion de import en gender.dart

--- lib/models/gender.dart
+++ lib/models/gender.dart
@@ -1,34 +1,12 @@
+import 'package:flutter/material.dart';
+
 enum Gender {
   male,
   female,
+  nonBinary,
   other,
-  preferNotToSay;
-
-  String get label => switch (this) {
-        Gender.male => 'Masculino',
-        Gender.female => 'Femenino',
-        Gender.other => 'Otro',
-        Gender.preferNotToSay => 'Prefiero no decir',
-      };
-
-  String get defaultBackgroundHex => switch (this) {
-        Gender.female => '#E85D9C',
-        _ => '#E0E0E0',
-      };
+  preferNotToSay,
 }
-
-import 'package:flutter/material.dart';
-
-extension GenderColorFix on Gender {
-  Color get color {
-    final str = toString().toLowerCase();
-    if (str.contains('female') || str.contains('mujer')) return Colors.pinkAccent;
-    if (str.contains('male') || str.contains('hombre')) return Colors.blueAccent;
-    return Colors.purpleAccent;
-  }
-}
-
-import 'package:flutter/material.dart';
 
 extension GenderColorFix on Gender {
   Color get color {
