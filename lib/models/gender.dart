143423f2b026fdd87ca26896bf60afbed6e8df42
DarkStar121-esp <villarruelsantyago@gmail.com>
2026-09-18 09:20:49 -0300

    Solucion integral de errores de compilacion y configuracion APK

--- lib/models/gender.dart
+++ lib/models/gender.dart
@@ -27,3 +27,14 @@ extension GenderColorFix on Gender {
     return Colors.purpleAccent;
   }
 }
+
+import 'package:flutter/material.dart';
+
+extension GenderColorFix on Gender {
+  Color get color {
+    final str = toString().toLowerCase();
+    if (str.contains('female') || str.contains('mujer')) return Colors.pinkAccent;
+    if (str.contains('male') || str.contains('hombre')) return Colors.blueAccent;
+    return Colors.purpleAccent;
+  }
+}
